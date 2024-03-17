6d9dd553694f7181e56c919d849ef9950841638a
Ryochi Tanaka <ryo1.t.zubu@gmail.com>
2024-03-17 21:56:53 +1000

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+import { resolve } from 'path'
+
+export default defineNuxtConfig({
+  alias: {
+    '@': resolve(__dirname, '/'),
+  },
+  css: ['~/assets/main.scss'],
+  devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
